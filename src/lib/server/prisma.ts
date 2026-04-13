95d94bd0717720e6da5bde23826be26056579a6d
andressb98 <andressolisbautista15@gmail.com>
2026-04-13 06:58:42 -0600

    fix: update Prisma dependencies to version 5.20.0 and improve type definitions in prisma.ts

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,7 +1,6 @@
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as { prisma: any };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
