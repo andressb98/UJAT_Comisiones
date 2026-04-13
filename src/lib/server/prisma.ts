e741e4152a95ce88729ee079b788838a90047398
andressb98 <andressolisbautista15@gmail.com>
2026-04-13 06:53:41 -0600

    refactor: remove prisma.config.ts and update schema.prisma to use env for DATABASE_URL

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,17 +1,8 @@
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
-import { env } from '$env/dynamic/private';
 
 const globalForPrisma = globalThis as unknown as { prisma: any };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: env.DATABASE_URL
-      }
-    }
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
